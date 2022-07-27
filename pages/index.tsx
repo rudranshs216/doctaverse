b20c93255498148e9b246959ae7951cf6429f1e7
Divyansh Sharma <divyanshsharma@Divyanshs-MacBook-Air.local>
2022-07-27 17:30:45 +0530

    four

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import HeroComponent from "../Components/HeroComponent"
 import Nav from '../Components/Nav'
 
@@ -13,7 +14,24 @@ const Home: NextPage = () => {
       </Head>
       <div>
         <h1 className='text-5xl underline'>Hello</h1>
-        <Nav />
+        <div className='h-full pb-10 pt-20 lg:py-20 w-full bg-[#0084ff] flex flex-col justify-around items-center flex-wrap lg:h-full lg:flex-row'>
+            <img className='w-[100%] max-w-[500px]' src="assets/images/main.png" />
+            <div className="w-[100%] mt-10 p-4 sm:w-[500px] lg:mt-0">
+                <h1 className='text-white text-3xl font-bold md:text-4xl lg:text-5xl'>One and Only Place for All Doctors Tool</h1>
+                <p className='text-white text-l my-5 font-semibold md:text-xl lg:text-2xl'>Doctaverse app includes all the medical tools needed by a pediatrician on duty.</p>
+                <p className='text-white text-md my-5 md:text-l lg:text-xl'>It is a pocket buddy for pediatricians containing information on pediatric medical calculators,drug dosage, diagnostic algorithms,treatment algorithms, emergency treatment, ECG and different guidelines.</p>
+                <div>
+                    <Link href="https://apps.apple.com/eg/app/doctaverse-pediatrics/id1616885718">
+                    <button className='px-6 py-3 bg-[#2c343c] text-white border-2 border-[#0084ff] rounded my-5 mr-5 hover:border-black hover:bg-white hover:text-black'>App Store</button>
+                    </Link>
+                    <Link href="https://play.google.com/store/apps/details?id=com.doctaverse.doctors.community.diagnostics.calculators.drugdoses">
+                    <button className='px-6 py-3 rounded bg-white border-2 border-[#0084ff]  hover:bg-[#0099ff]/50 hover:border-white hover:text-white'>Google Play</button>
+                    </Link>
+                </div>
+
+            </div>
+
+        </div>
       </div>
 
     </>
