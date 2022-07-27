bf999131719f1b593c11d8da875e318a299edfa2
Divyansh Sharma <divyanshsharma@Divyanshs-MacBook-Air.local>
2022-07-27 17:47:10 +0530

    seven

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import AppOne from "../Components/AppOne"
 import AppTwo from "../Components/AppTwo"
 import Feature from "../Components/Feature"
 import Footer from "../Components/Footer"
-import HeroComponent from "../Components/HeroComponent"
 import Navbar from '../Components/Navbar'
 import TestimonialComponent from "../Components/TestimonialComponent"
 
@@ -19,8 +18,26 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        
-        <HeroComponent />
+        <Navbar />
+
+      <div className='h-full pb-10 pt-20 lg:py-20 w-full bg-[#0084ff] flex flex-col justify-around items-center flex-wrap lg:h-full lg:flex-row'>
+            <img className='w-[100%] max-w-[500px]' src="assets/images/main.png" />
+            <div className="w-[100%] mt-10 p-4 sm:w-[500px] lg:mt-0">
+                <h1 className='text-white text-3xl font-bold md:text-4xl lg:text-5xl'>One and Only Place for All Doctors Tool</h1>
+                <p className='text-white text-l my-5 font-semibold md:text-xl lg:text-2xl'>Doctaverse app includes all the medical tools needed by a pediatrician on duty.</p>
+                <p className='text-white text-md my-5 md:text-l lg:text-xl'>It is a pocket buddy for pediatricians containing information on pediatric medical calculators,drug dosage, diagnostic algorithms,treatment algorithms, emergency treatment, ECG and different guidelines.</p>
+                <div>
+                    <Link href="https://apps.apple.com/eg/app/doctaverse-pediatrics/id1616885718">
+                    <button className='px-6 py-3 bg-[#2c343c] text-white border-2 border-[#0084ff] rounded my-5 mr-5 hover:border-black hover:bg-white hover:text-black'>App Store</button>
+                    </Link>
+                    <Link href="https://play.google.com/store/apps/details?id=com.doctaverse.doctors.community.diagnostics.calculators.drugdoses">
+                    <button className='px-6 py-3 rounded bg-white border-2 border-[#0084ff]  hover:bg-[#0099ff]/50 hover:border-white hover:text-white'>Google Play</button>
+                    </Link>
+                </div>
+
+            </div>
+
+        </div>
         <Feature />
         <AppOne imgSrc="assets/images/dashboard.png" heading="Doctor's Dashboard" paragraph="Plateform for doctors to see 10 patients in 1 hour and can genrate prescription just by their voice" />
         <AppTwo imgSrc="assets/images/cal.png" heading="Medical Calculator" paragraph="All the pediatric calculators which students use on a day to day basis. Calculators like GIR calculation, Pediatric BP Centiles (Hypertension), Anthropometry,Hyponatremic dehydration calculation,etc are part of this app. Pediatric Medical Students can calculate any value within 5 minutes." />
