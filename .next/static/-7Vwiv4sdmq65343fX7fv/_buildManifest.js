170673e65e773b80e8026fcbf112782cfba2437c
Divyansh Sharma <divyanshsharma@Divyanshs-MacBook-Air.local>
2022-07-28 10:03:51 +0530

    Final DoctaVerse

---
+++ .next/static/-7Vwiv4sdmq65343fX7fv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/72-08ea39c161688754.js","static/chunks/pages/index-c52756e739bf2d4d.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
